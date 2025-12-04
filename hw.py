64e62d8c59449707ccc5663b35a184c6ce7385d5
qaaziabdullah-eng <qaaziabdullah@gmail.com>
2025-12-04 18:06:38 +0500

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,12 @@
+#Summer times
+
+season=input("Enter season:summer/winter/rainy:")
+
+if season=="summer":
+    print("You can wear Cotton")
+
+elif season=="rainy":
+    print("You can wear raincoat")
+
+elif season=="winter":
+    print("You can wear jacket")
